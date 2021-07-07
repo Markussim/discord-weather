fe4c21433e4c35244fa4b458cc1863df21aa2b57
Linus Romland <romlandlinus@gmail.com>
2021-07-07 23:31:55 +0200

    wokring wok commands

--- server/app.js
+++ server/app.js
@@ -4,13 +4,14 @@ require('dotenv').config()
 
 const guildId = '554977304665784325'
 const client = new DiscordJS.Client()
+const mongoURL = process.env.MONGOURL || "mongodb://localhost:27017/";
 
 client.on('ready', () => {
   new WOKCommands(client, {
     commandsDir: 'commands',
     testServers: [guildId],
-    showWarns: false,
-  })
+    showWarns: true,
+  }).setMongoPath(mongoURL)
 })
 
 client.login(process.env.TOKEN)
--- server/commands/ping.js
+++ server/commands/ping.js
@@ -1,7 +1,16 @@
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-  slash: true,
+  slash: 'both',
   testOnly: true,
-  description: 'A simple ping pong command!!!',
+  description: 'A simple ping pong!!!',
+  callback: ({ message, args }) => {
+    const embed = new MessageEmbed().setTitle('PingPong!').setDescription('pong')
+    if (message) {
+      console.log(message)
+      message.reply('', { embed })
+    }
+
+    return embed
+  },
 }
